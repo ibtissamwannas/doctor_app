fe58dbd433d18df5e6437394d309055ac4addacc
ibtissam <ibtissamwannas21@gmail.com>
2025-01-02 16:43:08 +0200

    -translate the login design into flutter code -added spacing file for sizedbox

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,5 +1,5 @@
 import 'package:doctor_app/core/routing/routes.dart';
-import 'package:doctor_app/features/login/ui/screens/login_screen.dart';
+import 'package:doctor_app/features/login/ui/login_screen.dart';
 import 'package:doctor_app/features/onborading/onboarding_screen.dart';
 import 'package:flutter/material.dart';
 
