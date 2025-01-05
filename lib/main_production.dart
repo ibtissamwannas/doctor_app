78f5e4317f98ab8ea0ee0ce2d8a8d2c324a1d559
ibtissam <ibtissamwannas21@gmail.com>
2025-01-05 14:48:06 +0200

    - setup flavor for both development and production environments

---
+++ lib/main_production.dart
@@ -0,0 +1,16 @@
+import 'package:doctor_app/core/di/dependency_injection.dart';
+import 'package:doctor_app/core/routing/app_router.dart';
+import 'package:doctor_app/doc_app.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  setUpGetIt();
+  await ScreenUtil.ensureScreenSize();
+  runApp(
+    DocApp(
+      appRouter: AppRouter(),
+    ),
+  );
+}
